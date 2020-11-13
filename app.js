56b7788cc7b3f2565a0f3cf46e4c26c797821626
AmberG <48540819+amberglembin@users.noreply.github.com>
2020-11-12 17:50:58 -0800

    game completed

--- app.js
+++ app.js
@@ -1,68 +1,114 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     //card options
-const cardArray = [
-    {
-        name: 'cat',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/616/616430.svg'
-    },
-    {
-        name: 'cat',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/616/616430.svg'
-    },
-    {
-        name: 'elephant',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/3069/3069224.svg'
-    },
-    {
-        name: 'elephant',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/3069/3069224.svg'
-    },
-    {
-        name: 'clownfish',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/875/875011.svg'
-    },
-    {
-        name: 'clownfish',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/875/875011.svg'
-    },
-    {
-        name: 'hounddog',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/2965/2965100.svg'
-    },
-    {
-        name: 'hounddog',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/2965/2965100.svg'
-    },
-    {
-        name: 'cow',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/2395/2395796.svg'
-    },
-    {
-        name: 'cow',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/2395/2395796.svg'
-    },
-    {
-        name: 'hen',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/1864/1864499.svg'
-    },
-    {
-        name: 'hen',
-        img: 'https://www.flaticon.com/svg/static/icons/svg/1864/1864499.svg'
-    }
+    const cardArray = [
+        {
+            name: 'cat',
+            img: 'images/cat.png'
+        },
+        {
+            name: 'cat',
+            img: 'images/cat.png'
+        },
+        {
+            name: 'elephant',
+            img: 'images/elephant.png'
+        },
+        {
+            name: 'elephant',
+            img: 'images/elephant.png'
+        },
+        {
+            name: 'clownfish',
+            img: 'images/clown-fish.png'
+        },
+        {
+            name: 'clownfish',
+            img: 'images/clown-fish.png'
+        },
+        {
+            name: 'dog',
+            img: 'images/dog.png'
+        },
+        {
+            name: 'dog',
+            img: 'images/dog.png'
+        },
+        {
+            name: 'hen',
+            img: 'images/hen.png'
+        },
+        {
+            name: 'hen',
+            img: 'images/hen.png'
+        },
+        {
+            name: 'owl',
+            img: 'images/owl.png'
+        },
+        {
+            name: 'owl',
+            img: 'images/owl.png'
+        }
+    ]
 
-]
-})
+cardArray.sort(() => 0.5 - Math.random())
 
 const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+var cardsChosen = []
+var cardsChosenId = []
+var cardsWon = []
 
 //create gameboard
 function createBoard() {
-    for (let i = 0; i < cardArray.length; i++){
+    for (let i = 0; i < cardArray.length; i++) {
         var card = document.createElement('img')
-        card.setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/2913/2913456.svg')
+        card.setAttribute('src', 'images/forestOutline.png')
         card.setAttribute('data-id', i)
-        // card.addEventListener('click', flipcard)
+        card.addEventListener('click', flipCard)
         grid.appendChild(card)
     }
 }
+
+
+//check for matches
+function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match')
+        cards[optionOneId].setAttribute('src', 'images/forestColor.png')
+        cards[optionTwoId].setAttribute('src', 'images/forestColor.png')
+        cardsWon.push(cardsChosen)
+
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/forestOutline.png')
+        cards[optionTwoId].setAttribute('src', 'images/forestOutline.png')
+        alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if (cardsWon.length === cardsArray.length/2) {
+        resultDisplay.textContent = 'Congratulations! You found them all!'
+    } 
+}
+
+//flip your card
+function flipCard() {
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2){
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+
+createBoard()
+
+
+})
